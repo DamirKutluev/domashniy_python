de25dbf8ed2afbbe0a28ef29752f982745533e3a
damir <rentlkent543@mail.ru>
2023-11-20 10:00:28 +0500

    commit после первого слияния

--- file_master/master.py
+++ file_master/master.py
@@ -2,6 +2,6 @@ def plus(a, b):
     return a - b
 
 
-a = int(input("введите число а: "))
-b = int(input("введите число b: "))
-print(plus(a, b))
+a1 = int(input("введите число а: "))
+b1 = int(input("введите число b: "))
+print(plus(a1, b1))
